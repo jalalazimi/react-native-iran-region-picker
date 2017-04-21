fbfbd3579c8a0d08e78e256186adfe661a57d584
J.Azimi <jalalazimi@gmail.com>
2017-04-21 12:09:53 +0430

    update android and ios sample file

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -1,77 +1,79 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, TextInput, AppRegistry } from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet, Text, TouchableOpacity, TextInput, AppRegistry} from 'react-native';
 import IranRegionWheelPicker from './lib/index';
 
 export default class IranRegionModalPickerTest extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      region1: '',
-      isPickerVisible: false,
-      region2: ''
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            region1: '',
+            isPickerVisible: false,
+            region2: ''
+        };
+    }
 
-  _onPress2Show() {
-    this.setState({ isPickerVisible: true });
-  }
-  _onPressCancel() {
-    this.setState({ isPickerVisible: false });
-    console.log('cancel');
-  }
-  _onPressSubmit(params) {
-    this.setState({ isPickerVisible: false });
-    this.setState({ region2: `${params.province},${params.city},${params.area}` });
-  }
+    _onPress2Show() {
+        this.setState({isPickerVisible: true});
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <IranRegionWheelPicker
-          onSubmit={(params) => this.setState({ region1: `${params.province},${params.city},${params.area}` })}
-          onCancel={() => console.log('cancel')}
-        >
-          <TextInput
-            style={{ backgroundColor: '#FFF', width: 200, paddingVertical: 20, textAlign: 'center' }}
-            editable={false}
-            placeholder="انتخاب محله"
-            value={this.state.region1}
-          />
-        </IranRegionWheelPicker>
+    _onPressCancel() {
+        this.setState({isPickerVisible: false});
+        console.log('cancel');
+    }
+
+    _onPressSubmit(params) {
+        this.setState({isPickerVisible: false});
+        this.setState({region2: `${params.province},${params.city},${params.area}`});
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <IranRegionWheelPicker
+                    onSubmit={(params) => this.setState({region1: `${params.province},${params.city},${params.area}`})}
+                    onCancel={() => console.log('cancel')}
+                >
+                    <TextInput
+                        style={{
+                            borderWidth: 1,
+                            borderRadius: 3,
+                            borderColor: '#333',
+                            width: 200,
+                            paddingVertical: 20,
+                            textAlign: 'center'
+                        }}
+                        editable={false}
+                        placeholder="انتخاب موقعیت مکانی"
+                        value={this.state.region1}
+                    />
+                </IranRegionWheelPicker>
 
 
-        <IranRegionWheelPicker
-          isVisible={this.state.isPickerVisible}
-          navBtnColor={'red'}
-          selectedProvince={'تهران'}
-          selectedCity={'تهران'}
-          selectedArea={'تجریش'}
-          transparent
-          animationType={'fade'}
-          onSubmit={this._onPressSubmit.bind(this)}
-          onCancel={this._onPressCancel.bind(this)}
-        />
+                <IranRegionWheelPicker
+                    isVisible={this.state.isPickerVisible}
+                    navBtnColor={'red'}
+                    selectedProvince={'تهران'}
+                    selectedCity={'تهران'}
+                    selectedArea={'تجریش'}
+                    transparent
+                    animationType={'fade'}
+                    onSubmit={this._onPressSubmit.bind(this)}
+                    onCancel={this._onPressCancel.bind(this)}
+                />
 
-        <TouchableOpacity
-          activeOpacity={0.8}
-          style={{ marginTop: 50, backgroundColor: 'red', padding: 10, borderRadius: 6 }}
-          onPress={this._onPress2Show.bind(this)}
-        >
-          <Text style={{ color: 'white' }}>{this.state.region2 || 'انتخاب محله' }</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E1E1E1',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#E1E1E1',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => IranRegionModalPickerTest);
